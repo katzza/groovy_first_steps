f70440b37afc7b38d0092656ecb4d667f29d7690
emokeeva <ekaterina.mokeeva@t-systems.com>
2020-12-04 22:53:09 +0300

    >>

--- src/firstHello.groovy
+++ src/firstHello.groovy
@@ -1,8 +1,32 @@
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 
+//how to create groovy project in Idea   https://www.logicbig.com/tutorials/misc/groovy/intellij.html
+
+
 println "murr"
 
 
-String jsonStr = '{"id": "111"}'
-println new JsonBuilder(new JsonSlurper().parseText(jsonStr)).toPrettyString()
+String jsonStr = '{"target":{"ciDescriptor":{"type":"Subscription","localId":"keinFehlerErkannt","ngssmDomain":"smk"}}}'
+def slurper = new JsonSlurper().parseText(jsonStr)
+def localId = slurper.target.ciDescriptor.localId    //чтобы вытащить зачение нижнего уровня, нужно упомянуть все узлы
+//println(localId)
+String path
+switch (localId) {
+    case ("keinFehlerErkannt"):
+        path = 'C:/Users/emokeeva/Documents/Soap_ui_projects/messages/keinFehlerErkannt.txt'
+        break
+    case ("fehlerLinientechnik"):
+        path = 'C:/Users/emokeeva/Documents/Soap_ui_projects/messages/fehlerLinientechnik.txt'
+        break
+    case ("fehlerTechnik"):
+        path = 'C:/Users/emokeeva/Documents/Soap_ui_projects/messages/fehlerTechnik.txt'
+        break
+    default: println(3)
+}
+println(path)
+
+/*def jsonS = new JsonBuilder(new JsonSlurper().parseText(jsonStr)).toPrettyString()
+
+println(localId)*/
+
